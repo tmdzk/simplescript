fdd4039d27783f1d1899d64bd3becf3679975efb
Timothée Dzik <timdzik@fb.com>
2019-09-02 10:39:10 -0700

    test script

--- agw_baremetal_network.sh
+++ agw_baremetal_network.sh
@@ -1,13 +1,29 @@
-#!/bin/sh 
+#!/bin/sh
 
+# changing intefaces name
 sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
 
+# changing interface name
 grub-mkconfig -o /boot/grub/grub.cfg
 sed -i 's/enp1s0/eth0/g' /etc/network/interfaces
 
+# configuring eth1
 echo "auto eth1
 iface eth1 inet static
 address 10.0.2.1
 netmask 255.255.255.0" > /etc/network/interfaces.d/eth1
+
+# Installing prerequesites
+apt install -y sudo python-minimal aptitude
+
+# Making magma sudoer
+adduser magma sudo
+echo "magma ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# Making sure .ssh is created in magma user
+mkdir -p /home/magma/.ssh
+chown magma:magma /home/magma/.ssh
+# Removing useless Kernel version
+apt remove -y linux-image-4.9.0-11-amd64
 
 reboot
---
+++ test.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+# Setting up env variable, user and project path
+MAGMA_ROOT="/home/magma"
+MAGMA_USER="timodzik"
+AGW_INSTALL_CONFIG="/etc/systemd/system/multi-user.target.wants/agw_installation.service"
+# Testing if the right Kernel Version is installed and $MAGMA_USER is sudoers
+if `uname -r` -ne "4.9.0-9-amd64" && ! grep -q "$MAGMA_USER ALL=(ALL) NOPASSWD:ALL" /etc/sudoers; then
+  if `ping -c 1 -I enp1s0 8.8.8.8 &> /dev/null && echo "ok"` -ne "ok"; then
+    echo "enp1s0 (eth0) is not connected to internet, please double check your plugged wires."
+    exit 1
+  fi
+  # changing intefaces name
+  sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
+  # changing interface name
+  grub-mkconfig -o /boot/grub/grub.cfg
+  sed -i 's/enp1s0/eth0/g' /etc/network/interfaces
+
+  # configuring eth1
+  echo "auto eth1
+  iface eth1 inet static
+  address 10.0.2.1
+  netmask 255.255.255.0" > /etc/network/interfaces.d/eth1
+
+  # As 4.9.0-9-amd64 has been removed from the current deb repo we're temporary using a snapshot
+  if ! grep -q "deb http://snapshot.debian.org/archive/debian/20190801T025637Z" /etc/apt/sources.list; then
+    echo "deb http://snapshot.debian.org/archive/debian/20190801T025637Z stretch main non-free contrib" >> /etc/apt/sources.list
+  fi
+
+  # Update apt
+  apt update
+  # Installing prerequesites
+  apt install -y sudo python-minimal aptitude linux-image-4.9.0-9-amd64 linux-headers-4.9.0-9-amd64
+  # Removing dev repository snapshot from source.list
+  sed -i '/20190801T025637Z/d' /etc/apt/sources.list
+
+  # Making magma a sudoer
+  adduser $MAGMA_USER sudo
+  echo "$MAGMA_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+  # Making sure .ssh is created in magma user
+  mkdir -p /home/$MAGMA_USER/.ssh
+  chown $MAGMA_USER:$MAGMA_USER /home/$MAGMA_USER/.ssh
+  # Removing incompatible Kernel version
+  apt remove -y linux-image-4.9.0-11-amd64
+  chmod 644 $AGW_INSTALL_CONFIG
+  # echo the the service config
+  echo "[Unit]
+Description=AGW Installation
+
+[Service]
+Type=oneshot
+ExecStart=/bin/sh /root/agw_install.sh
+User=root
+Group=root
+
+[Install]
+WantedBy=multi-user.target" > $AGW_INSTALL_CONFIG
+  reboot
+else
+  apt-get update
+  apt-get -y install curl make virtualenv zip rsync git software-properties-common python3-pip python-dev
+  alias python=python3
+  # Installing ansible.
+  pip3 install ansible
+  # Cloning magma.
+  git clone https://github.com/facebookincubator/magma.git /home/$MAGMA_USER/magma
+  # Setting up deploy path
+  DEPLOY_PATH="/home/$MAGMA_USER/magma/lte/gateway/deploy"
+  # Generating a localhost hostfile.
+  echo "[ovs_build]
+  127.0.0.1 ansible_connection=local
+  [ovs_deploy]
+  127.0.0.1 ansible_connection=local" > $DEPLOY_PATH/agw_hosts
+  # Triggering ovs_build in order to build custom patches.
+  su - $MAGMA_USER -c "ansible-playbook -e \"MAGMA_ROOT='/home/$MAGMA_USER/magma' OUTPUT_DIR='/tmp'\" -i $DEPLOY_PATH/agw_hosts $DEPLOY_PATH/ovs_build.yml"
+  # Triggering ovs_deploy in order deploy magma.
+  su - $MAGMA_USER -c "ansible-playbook -e \"PACKAGE_LOCATION='/tmp'\" -i $DEPLOY_PATH/agw_hosts $DEPLOY_PATH/ovs_deploy.yml"
+  # deleting boot script
+  rm -rf $AGW_INSTALL_CONFIG
+  # removing ansible from the freshly installed agw
+  pip3 uninstall --yes ansible
+  # Final messages magma status and end message
+  service magma@* status
+  echo "AGW installation is done."
+fi
